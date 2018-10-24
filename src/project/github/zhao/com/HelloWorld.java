877f5403e6654030037d41cfeacff189bb81840a
赵云龙 <@LAPTOP-2P1960P0>
2018-10-24 09:56:34 +0800

    10.24日修改文件

--- src/project/github/zhao/com/HelloWorld.java
+++ src/project/github/zhao/com/HelloWorld.java
@@ -1,9 +1,31 @@
 package project.github.zhao.com;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("打印东西了！");
+		System.out.println("打印当前时间！");
+		long time = System.currentTimeMillis();
+		System.out.println(time/1000/60%60);
+		System.out.println(time/1000/60/60%12 + 8);
+		
+		
+		
+		
+		
+		
+		
+		
+//		System.out.println("打印东西了！");
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("请输入：");
+//		if(input.hasNext()) {
+//			System.out.println("输入的为字符串");
+//		}else {
+//			System.out.println("输入有误");
+//			
+//		}
 	}
 
 }
